e5ba796e0f0136cce2f96fdc8f37f44509113a28
liuzhijian <18576746147@163.com>
2021-10-26 22:41:20 +0800

    feat: css

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,23 @@ module.exports = {
   base: '/',
   title: '追风',
   description: 'Just playing around',
+  themeConfig: {
+    //logo: '/assets/img/logo.jpg', // 导航栏 Logo
+    sidebar: 'auto', // 自动生成侧栏
+    nav: [ // 导航栏链接
+      {
+        text: '前端',
+        items: [
+          {text: 'CSS', link: '/views/css/index.md'},
+          {text: 'JS', link: '/language/japanese/'},
+          {text: 'Vue', link: '/language/japanese/'},
+          {text: 'Webpack', link: '/language/japanese/'}
+        ]
+      },
+      { text: '面试题', link: '/guide/' },
+      { text: 'GitHub', link: 'https://github.com/liu464847593' },
+    ]
+  },
   configureWebpack: {
     resolve: {
       alias: {
